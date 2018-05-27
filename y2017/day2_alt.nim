cc091872e8f89eaaa2a3ca813ce38a7a9c69f174
Antonis G <capoiosct@gmail.com>
2018-05-27 14:57:39 +0300

    Remove branching

--- y2017/day2_alt.nim
+++ y2017/day2_alt.nim
@@ -30,37 +30,37 @@ proc initSpreadsheet(data: seq[seq[string]]): Spreadsheet =
    inner = 0
    var count = 0
    for i, d in data:
-      result.offsets[i] = inner
       inner += d.len
+      result.offsets[i] = inner
       for s in d:
          result.data[count] = parseInt(s)
          count.inc
 
 proc sortEachRow(s: var Spreadsheet) =
+   var prev = 0
    for i in 0 ..< s.offsets.len:
-      if i + 1 < s.offsets.len:
-         sort(toOpenArray(s.data, s.offsets[i], s.offsets[i + 1] - 1), cmp)
-      else:
-         sort(toOpenArray(s.data, s.offsets[i], s.data.len - 1), cmp)
+      sort(toOpenArray(s.data, prev, s.offsets[i] - 1), cmp)
+      prev = s.offsets[i]
 
 template notSortedRows(s) =
-   assert isSorted(toOpenArray(s.data, 0, s.offsets[1] - 1), cmp)
+   assert isSorted(toOpenArray(s.data, 0, s.offsets[0] - 1), cmp)
 
 proc checksum0(s: Spreadsheet): int =
    notSortedRows(s)
+   var prev = 0
    for i in 0 ..< s.offsets.len:
-      if i + 1 < s.offsets.len:
-         result += s.data[s.offsets[i + 1] - 1] - s.data[s.offsets[i]]
-      else:
-         result += s.data[s.data.len - 1] - s.data[s.offsets[i]]
+      result += s.data[s.offsets[i] - 1] - s.data[prev]
+      prev = s.offsets[i]
 
 # proc checksum(s: Spreadsheet): int =
 #    notSortedRows(s)
-#    for row in s:
+#    for i in 0 ..< s.offsets.len:
 #       var d = 0
-#       for p in combinations(row, 2):
+#       var prev = 0
+#       for p in combinations(toOpenArray(s.data, prev, s.offsets[i] - 1), 2):
 #          if p[1] mod p[0] == 0:
 #             d = p[1] div p[0]
+#       prev = s.offsets[i]
 #       result += d
 
 # let input = "5 1 9 5\n7 5 3\n2 4 6 8\n"
@@ -68,5 +68,6 @@ proc checksum0(s: Spreadsheet): int =
 let data = readData("input2.txt")
 var s = initSpreadsheet(data)
 s.sortEachRow()
+echo s
 echo s.checksum0()
 # echo s.checksum()
