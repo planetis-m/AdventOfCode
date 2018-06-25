c6ca803d3e7f40aa8b869c23364923f2148c2b37
Antonis G <capoiosct@gmail.com>
2018-06-25 21:58:07 +0300

    Fix

--- y2017/day2_alt.nim
+++ y2017/day2_alt.nim
@@ -2,7 +2,7 @@ import strutils, algorithm #, combinatorics
 
 type
    Spreadsheet = object
-      offsets: seq[int]
+      lens: seq[int]
       data: seq[int]
 
 proc readData(fname: string): seq[seq[string]] =
@@ -26,41 +26,52 @@ proc initSpreadsheet(data: seq[seq[string]]): Spreadsheet =
    for d in data:
       inner += d.len
    result.data = newSeq[int](inner)
-   result.offsets = newSeq[int](data.len)
+   result.lens = newSeq[int](data.len)
    inner = 0
    var count = 0
    for i, d in data:
       inner += d.len
-      result.offsets[i] = inner
+      result.lens[i] = inner
       for s in d:
          result.data[count] = parseInt(s)
          count.inc
 
-proc sortEachRow(s: var Spreadsheet) =
+proc row(s: Spreadsheet, i: Natural): seq[int] =
+   s.data[if i > 0: s.lens[i - 1] else: 0 ..< s.lens[i]]
+
+iterator rows(s: Spreadsheet): seq[int] =
    var prev = 0
-   for i in 0 ..< s.offsets.len:
-      sort(toOpenArray(s.data, prev, s.offsets[i] - 1), cmp)
-      prev = s.offsets[i]
+   for i in 0 ..< s.lens.len:
+      let curr = s.lens[i]
+      yield s.data[prev ..< curr]
+      prev = curr
+
+iterator mrows(s: var Spreadsheet): auto =
+   var prev = 0
+   for i in 0 ..< s.lens.len:
+      let curr = s.lens[i]
+      yield toOpenArray(s.data, prev, curr - 1)
+      prev = curr
+
+proc sortEachRow(s: var Spreadsheet) =
+   for r in mrows(s):
+      sort(r, cmp)
 
 template notSortedRows(s) =
-   assert isSorted(toOpenArray(s.data, 0, s.offsets[0] - 1), cmp)
+   assert isSorted(s.row(0), cmp)
 
 proc checksum0(s: Spreadsheet): int =
    notSortedRows(s)
-   var prev = 0
-   for i in 0 ..< s.offsets.len:
-      result += s.data[s.offsets[i] - 1] - s.data[prev]
-      prev = s.offsets[i]
+   for r in rows(s):
+      result += r[^1] - r[0]
 
 # proc checksum(s: Spreadsheet): int =
 #    notSortedRows(s)
-#    for i in 0 ..< s.offsets.len:
+#    for r in rows(s):
 #       var d = 0
-#       var prev = 0
-#       for p in combinations(toOpenArray(s.data, prev, s.offsets[i] - 1), 2):
+#       for p in combinations(r, 2):
 #          if p[1] mod p[0] == 0:
 #             d = p[1] div p[0]
-#       prev = s.offsets[i]
 #       result += d
 
 # let input = "5 1 9 5\n7 5 3\n2 4 6 8\n"
@@ -68,6 +79,5 @@ proc checksum0(s: Spreadsheet): int =
 let data = readData("input2.txt")
 var s = initSpreadsheet(data)
 s.sortEachRow()
-echo s
 echo s.checksum0()
 # echo s.checksum()
